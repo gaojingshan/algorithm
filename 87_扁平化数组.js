f3686b3345d602275aa1bdabd42248c00d6e41cc
高境杉 <1359287622@qq.com>
2020-03-31 18:45:40 +0800

    不要放弃

--- 87_扁平化数组.js
+++ 87_扁平化数组.js
@@ -1,1 +1,18 @@
-// 
+// 编写函数flatten(arr)，它的功能是可以扁平化数组。比如可以将数组[[1,[2,3],4,5],[6,7]]传入数组，则返回[1,2,3,4,5,6,7]
+// 递归
+var arr = [
+  [1, [2, 3], 4, 5],
+  [6, 7],
+];
+var result = [];
+function flatten(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      flatten(arr[i]);
+    } else {
+      result.push(arr[i]);
+    }
+  }
+}
+flatten(arr);
+console.log(result);
